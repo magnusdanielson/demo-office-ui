17f9f4c1fef35209809490655fc6e05231fbbe38
MagnusDanielson <magnusdanielson@hotmail.com>
2018-12-03 00:40:11 +0100

    funkar

--- aurelia_project/tasks/run.ts
+++ aurelia_project/tasks/run.ts
@@ -1,54 +1,47 @@
-import * as gulp from 'gulp';
-import * as browserSync from 'browser-sync';
-import * as historyApiFallback from 'connect-history-api-fallback/lib';
-import {CLIOptions} from 'aurelia-cli';
+import {config} from './build';
+import configureEnvironment from './environment';
+import * as webpack from 'webpack';
+import * as Server from 'webpack-dev-server';
 import * as project from '../aurelia.json';
-import build from './build';
-import watch from './watch';
+import {CLIOptions, reportWebpackReadiness} from 'aurelia-cli';
+import * as gulp from 'gulp';
 
-let serve = gulp.series(
-  build,
-  done => {
-    browserSync({
-      online: false,
-      open: false,
-      port: 9000,
-      logLevel: 'silent',
-      server: {
-        baseDir: [project.platform.baseDir],
-        middleware: [historyApiFallback(), function(req, res, next) {
-          res.setHeader('Access-Control-Allow-Origin', '*');
-          next();
-        }]
-      }
-    }, function (err, bs) {
-      if (err) return done(err);
-      let urls = bs.options.get('urls').toJS();
-      log(`Application Available At: ${urls.local}`);
-      log(`BrowserSync Available At: ${urls.ui}`);
-      done();
-    });
+function runWebpack(done) {
+  // https://webpack.github.io/docs/webpack-dev-server.html
+  let opts = {
+    host: 'localhost',
+    publicPath: config.output.publicPath,
+    filename: config.output.filename,
+    hot: project.platform.hmr || CLIOptions.hasFlag('hmr'),
+    port: project.platform.port,
+    contentBase: config.output.path,
+    historyApiFallback: true,
+    open: project.platform.open,
+    stats: {
+      colors: require('supports-color')
+    },
+    https: config.devServer.https
+  } as any;
+
+  if (project.platform.hmr || CLIOptions.hasFlag('hmr')) {
+    config.plugins.push(new webpack.HotModuleReplacementPlugin());
+    config.entry.app.unshift(`webpack-dev-server/client?http://${opts.host}:${opts.port}/`, 'webpack/hot/dev-server');
   }
+
+  const compiler = webpack(config);
+  let server = new Server(compiler, opts);
+
+  server.listen(opts.port, opts.host, function(err) {
+    if (err) throw err;
+
+    reportWebpackReadiness(opts);
+    done();
+  });
+}
+
+const run = gulp.series(
+  configureEnvironment,
+  runWebpack
 );
 
-function log(message) {
-  console.log(message);
-}
-
-function reload() {
-  log('Refreshing the browser');
-  browserSync.reload();
-}
-
-let run;
-
-if (CLIOptions.hasFlag('watch')) {
-  run = gulp.series(
-    serve,
-    done => { watch(reload); done(); }
-  );
-} else {
-  run = serve;
-}
-
-export default run;
+export { run as default };
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,30 @@
+/// <reference types="aurelia-loader-webpack/src/webpack-hot-interface"/>
+// we want font-awesome to load as soon as possible to show the fa-spinner
 import {Aurelia} from 'aurelia-framework'
 import environment from './environment';
-import {PLATFORM} from 'aurelia-framework';
+import {PLATFORM} from 'aurelia-pal';
+import * as Bluebird from 'bluebird';
+
+// remove out if you don't want a Promise polyfill (remove also from webpack.config.js)
+//Bluebird.config({ warnings: { wForgottenReturn: false } });
 
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
-    .plugin(PLATFORM.moduleName('@dunite/au-office-ui')) //does not crasch
-    //.plugin(PLATFORM.moduleName('sample-plugin2')) //does not crasch
-    .feature('resources');
+    .feature(PLATFORM.moduleName('resources/index'));
 
-  if (environment.debug) {
-    aurelia.use.developmentLogging();
-  }
+  // Uncomment the line below to enable animation.
+  // aurelia.use.plugin(PLATFORM.moduleName('aurelia-animator-css'));
+  // if the css animator is enabled, add swap-order="after" to all router-view elements
+
+  // Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
+  // aurelia.use.plugin(PLATFORM.moduleName('aurelia-html-import-template-loader'));
+
+  aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
 
   if (environment.testing) {
-    aurelia.use.plugin('aurelia-testing');
+    aurelia.use.plugin(PLATFORM.moduleName('aurelia-testing'));
   }
 
-  aurelia.start().then(() => aurelia.setRoot());
+  return aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app')));
 }
